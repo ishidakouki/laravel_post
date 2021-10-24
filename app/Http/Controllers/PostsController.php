e40896fc7edfb559a135577abdda766d427e50cd
haruna-ishii <harunyyyn.d16@gmail.com>
2021-10-24 22:04:20 +0900

    add:新規投稿ページ実装

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 
 use App\User;
 use App\Post;
@@ -13,5 +14,19 @@ class PostsController extends Controller
     {
         $posts = Post::orderBy('created_at', 'desc')->get();
         return view('posts.index', ['posts' => $posts]);  
+    }
+    public function create()
+    {
+        return view('posts.create');
+    }
+    public function store(PostRequest $request)
+    {
+        $post = new Post;
+        $post->title = $request->title;
+        $post->text = $request->text;
+        $post->user_id = \Auth::id();
+        $post->save();
+
+        return redirect()->route('index');
     }
 }
