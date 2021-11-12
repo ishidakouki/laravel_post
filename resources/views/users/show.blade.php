6bc3232031b135192d60d2e5cd128d9479131159
Yuta Takagi <yuta.jan.takagi@gmail.com>
2021-11-12 16:11:12 +0900

    ログアウトボタンもユーザー判定に含めた

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -18,13 +18,10 @@
 
                 <div class="col-12 mt-3">
                     @if ($user->id == Auth::id())
-                        <a class="btn btn-outline-dark common-btn btn-sm edit-profile-btn" href="{{ route('users.edit', Auth::id() ) }}"><i class="fas fa-user-edit"></i>プロフィール編集
-                        </a>
+                        <a class="btn btn-outline-dark common-btn btn-sm edit-profile-btn" href="{{ route('users.edit', Auth::id() ) }}"><i class="fas fa-user-edit"></i>プロフィール編集</a>
+                        <a class="btn btn-outline-dark common-btn btn-sm edit-profile-btn" rel="nofollow" data-method="POST" href="{{ route('logout') }}"><i class="fas fa-cog"></i>ログアウト</a>
+                        <form id="logout-form" action="" method="POST" style="display: none;">{{ csrf_field() }}</form>
                     @endif
-                    <a class="btn btn-outline-dark common-btn btn-sm edit-profile-btn" rel="nofollow" data-method="POST" href="{{ route('logout') }}"><i class="fas fa-cog"></i>ログアウト
-                    </a>
-                    <form id="logout-form" action="" method="POST" style="display: none;">{{ csrf_field() }}
-                    </form>
                 </div>
             </div>
         </div>
