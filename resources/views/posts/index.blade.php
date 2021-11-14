5b314e0a9bcf60d26f25763d603054f6c4e3379e
Yuta Takagi <yuta.jan.takagi@gmail.com>
2021-11-14 20:22:18 +0900

    編集,削除ボタンCSS修正,Auth判定化

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -58,24 +58,16 @@
                     <div class="container mt-4 text-left">
                         <div class="border-top p-1">    
                             <span>
-
-                                <div class="post_edit">
-                                    <form class="edit_button" method="get" action="#">
-                                            @csrf
-                                            
-                                            <button class="btn btn-primary btn-sm"><i class="far fa-edit"></i>編集</button>
-                                    </form>
-                                    <form class="edit_button" method="post" action="{{ route('comments.destroy', $comment->id )}}" accept-charset="UTF-8">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-danger btn-sm" rel="nofollow" ><i class="far fa-trash-alt"></i>削除</button>
-                                    </form>
-                                </div>
                                 @if (Auth::id() == $comment->user->id)
                                     <div class="post_edit">
                                         <form class="edit_button" method="get" action="{{ route('comments.edit', $comment->id ) }}">
                                             @csrf
                                             <button class="btn btn-primary btn-sm"><i class="far fa-edit"></i>編集</button>
+                                        </form>
+                                        <form class="edit_button" method="post" action="{{ route('comments.destroy', $comment->id )}}" accept-charset="UTF-8">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-danger btn-sm" rel="nofollow" ><i class="far fa-trash-alt"></i>削除</button>
                                         </form>
                                     </div>
                                     @endif
